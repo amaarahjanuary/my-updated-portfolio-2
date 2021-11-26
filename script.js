17fc356c82dac5188972382d47b2bb23ed7d23ff
Amaarah January <amaarahjanuary456@gmail.com>
2021-11-26 08:36:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// $(function() {
+//     $('.owl-carousel.testimonial-carousel').owlCarousel({
+//       nav: true,
+//       navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+//       dots: false,
+//       responsive: {
+//         0: {
+//           items: 1,
+//         },
+//         750: {
+//           items: 2,
+//         }
+//       }
+//     });
+//   });
+
+// $('.page_title').each(function(i, obj) {
+//   var page_title_text = $(obj).text(); 
+//   $(obj).text(''); //clear text
+//   $(obj).append('<div class="page_title_front">'+page_title_text+'</div>');
+//   $(obj).append('<div class="page_title_back">'+page_title_text+'</div>');
+// });
